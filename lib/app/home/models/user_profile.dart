a5e1d10097dc8232bc2718d7b18eacb5ea8d0cc5
Stefan Otte <stefan.otte@gmail.com>
2020-04-29 14:13:33 +0200

    Add more attributes to the user profile

--- lib/app/home/models/user_profile.dart
+++ lib/app/home/models/user_profile.dart
@@ -1,7 +1,21 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 part 'user_profile.freezed.dart';
 
-const List<String> allInterests = ["Ernaehrung", "Yoga", "Nebenwirkungen"];
+class ProfileChoices {
+  static const List<String> interests = [
+    "Ernaehrung",
+    "Yoga",
+    "Sport",
+    "Meditation",
+    "Nebenwirkungen"
+  ];
+
+  static const List<String> circumstances = [
+    "Kinder",
+    "Berufstaetig",
+    "Rentner",
+  ];
+}
 
 @freezed
 abstract class UserProfile implements _$UserProfile {
@@ -9,27 +23,42 @@ abstract class UserProfile implements _$UserProfile {
   const factory UserProfile({
     String id,
     String name,
+    int yearOfBirth,
     List<String> interests,
+    List<String> circumstances,
   }) = _UserProfile;
 
   Map<String, dynamic> toMap() {
     return {
       'name': name,
+      'yearOfBirth': yearOfBirth,
       'interests': interests,
+      'circumstances': circumstances,
     };
   }
 
   factory UserProfile.fromMap(Map<String, dynamic> data, String documentID) {
     if (data == null) {
-      return UserProfile(id: documentID, name: "", interests: []);
+      return UserProfile(
+          id: documentID,
+          name: "",
+          yearOfBirth: null,
+          interests: [],
+          circumstances: []);
     }
     assert(data["interests"] != null);
-    final List<String> _interests = List.from(data["interests"]);
+    assert(data["circumstances"] != null);
     final String _name = data["name"];
+    final int _yearOfBirth = data["yearOfBirth"];
+    final List<String> _interests = List.from(data["interests"]);
+    final List<String> _circumstances = List.from(data["circumstances"]);
+
     return UserProfile(
       id: documentID,
       name: _name,
+      yearOfBirth: _yearOfBirth,
       interests: _interests,
+      circumstances: _circumstances,
     );
   }
 }
