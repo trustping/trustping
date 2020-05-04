a491fa50a6b25d2786b22f757a6425f823e30702
Stefan Otte <stefan.otte@gmail.com>
2020-05-04 11:07:14 +0200

    Add listify helper

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,11 +1,14 @@
-/// Like python's dict.get("key", "default_value")
+/// Like python's dict.get("key", "default_value").
 extension DefaultMap<K, V> on Map<K, V> {
-  V get(K key, {V defaultValue}) {
-    print("$this");
-    if (this.containsKey(key)) {
-      return this[key];
-    } else {
-      return defaultValue;
-    }
+  V get(K key, V defaultValue) {
+    return this.containsKey(key) ? this[key] : defaultValue;
   }
+}
+
+/// Turn things into lists.
+List<T> listify<T>(dynamic listlike) {
+  if (listlike == null) return [].cast<T>();
+  if (listlike is List) return listlike.cast<T>();
+  if (listlike is Iterable) return List.from(listlike).cast<T>();
+  return [listlike].cast<T>();
 }
