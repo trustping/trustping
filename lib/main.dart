906c4030c240ba5b9781813588797a1bd3deeb4e
Stefan Otte <stefan.otte@gmail.com>
2020-05-23 11:46:09 +0200

    Use new theme for into pages

--- lib/main.dart
+++ lib/main.dart
@@ -63,9 +63,29 @@ class MyApp extends StatelessWidget {
 }
 
 ThemeData trustPingTheme() {
+  const MaterialColor tpColors = const MaterialColor(
+    0xFFFFFFFF,
+    const <int, Color>{
+      50: const Color(0xFFFFFFFF),
+      100: const Color(0xFFFFFFFF),
+      200: const Color(0xFFFFFFFF),
+      300: const Color(0xFFFFFFFF),
+      400: const Color(0xFFFFFFFF),
+      500: const Color(0xFFFFFFFF),
+      600: const Color(0xFFFFFFFF),
+      700: const Color(0xFFFFFFFF),
+      800: const Color(0xFFFFFFFF),
+      900: const Color(0xFFFFFFFF),
+    },
+  );
   return ThemeData(
     fontFamily: 'Inter',
-    // primarySwatch: Colors.yellow,
+    primarySwatch: tpColors,
     // accentColor: Colors.redAccent,
+    textTheme: TextTheme(
+      headline1: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w600),
+      headline6: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w400),
+      bodyText2: TextStyle(fontSize: 15.0),
+    ),
   );
 }
