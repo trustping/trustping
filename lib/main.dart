83cb41541e807adb979e5ceed8f17043eec39045
Stefan Otte <stefan.otte@gmail.com>
2020-05-23 09:56:34 +0200

    dummy

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,6 @@ import 'package:trust_ping_app/services/firebase_auth_service.dart';
 
 import 'app/home/models/user.dart';
 
-import 'package:firebase_auth/firebase_auth.dart';
-
 void main() {
   // runApp(MyAppDummy());
   runApp(
@@ -70,98 +68,4 @@ ThemeData trustPingTheme() {
     // primarySwatch: Colors.yellow,
     // accentColor: Colors.redAccent,
   );
-}
-
-// ########################################################################################
-// MIN EXMAPLE
-class MyAppDummy extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-      ),
-      home: LandingPage(),
-    );
-  }
-}
-
-class LandingPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<FirebaseUser>(
-      stream: FirebaseAuth.instance.onAuthStateChanged,
-      builder: (context, snapshot) {
-        print("$snapshot");
-        if (snapshot.connectionState == ConnectionState.active) {
-          FirebaseUser user = snapshot.data;
-          if (user == null) {
-            return SignInPage();
-          }
-          return HomePage();
-        } else {
-          return Scaffold(
-            body: Center(
-              child: CircularProgressIndicator(),
-            ),
-          );
-        }
-      },
-    );
-  }
-}
-
-class SignInPage extends StatelessWidget {
-  Future<void> _signInAnonymously() async {
-    try {
-      await FirebaseAuth.instance.signInAnonymously();
-    } catch (e) {
-      print(e); // TODO: show dialog with error
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Sign in')),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Sign in anonymously'),
-          onPressed: _signInAnonymously,
-        ),
-      ),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  Future<void> _signOut() async {
-    try {
-      await FirebaseAuth.instance.signOut();
-    } catch (e) {
-      print(e); // TODO: show dialog with error
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Page'),
-        actions: <Widget>[
-          FlatButton(
-            child: Text(
-              'Logout',
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.white,
-              ),
-            ),
-            onPressed: _signOut,
-          ),
-        ],
-      ),
-    );
-  }
 }
---
+++ test/ping_view_model_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+main() {
+  group('PingViewModel', () {
+    // test('createChatID', () {
+    //   expect(true, false);
+    // });
+  });
+}
