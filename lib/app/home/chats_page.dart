e59b6d035c0577e558c5093f2f3e50ec50931eae
Stefan Otte <stefan.otte@gmail.com>
2020-06-14 18:05:46 +0200

    Move Profile logic into profile tab (#15)

--- lib/app/home/chats_page.dart
+++ lib/app/home/chats_page.dart
@@ -1,9 +1,15 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'package:trust_ping_app/app/spaces.dart';
 import 'package:trust_ping_app/common_widgets/avatar.dart';
 import 'package:trust_ping_app/common_widgets/list_items_builder.dart';
+import 'package:trust_ping_app/common_widgets/platform_alert_dialog.dart';
+import 'package:trust_ping_app/common_widgets/platform_exception_alert_dialog.dart';
+import 'package:trust_ping_app/constants/strings.dart';
 import 'package:trust_ping_app/routing/router.gr.dart';
+import 'package:trust_ping_app/services/firebase_auth_service.dart';
 import 'package:trust_ping_app/services/firestore_database.dart';
 import 'package:trust_ping_app/theme.dart';
 
@@ -43,12 +49,12 @@ class _ChatsPageState extends State<ChatsPage> {
 
   List<Widget> _buildActions(BuildContext context) {
     return <Widget>[
-      IconButton(
-        icon: Icon(Icons.account_circle),
-        onPressed: () {
-          ExtendedNavigator.of(context).pushNamed(Routes.accountPage);
-        },
-      ),
+      // IconButton(
+      //   icon: Icon(Icons.account_circle),
+      //   onPressed: () {
+      //     ExtendedNavigator.of(context).pushNamed(Routes.accountPage);
+      //   },
+      // ),
       IconButton(
         icon: Icon(Icons.info),
         onPressed: () {
@@ -123,7 +129,7 @@ class _ChatsPageState extends State<ChatsPage> {
         ],
       );
     } else if (widgetIndex == 2) {
-      return Center(child: Text("TODO Profile Widget"));
+      return ProfileWidget();
     }
 
     final db = Provider.of<FirestoreDatabase>(context, listen: false);
@@ -138,6 +144,61 @@ class _ChatsPageState extends State<ChatsPage> {
         );
       },
     );
+  }
+}
+
+class ProfileWidget extends StatelessWidget {
+  const ProfileWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        vspace32,
+        vspace32,
+        Text("TODO Profile Widget"),
+        vspace32,
+        RaisedButton(
+          color: Style.yellow,
+          child: Text(
+            Strings.logout,
+            style: TextStyle(
+              fontSize: 18.0,
+              // color: Theme.of(context).accentColor,
+            ),
+          ),
+          onPressed: () => _confirmSignOut(context),
+        ),
+      ],
+    );
+  }
+
+  Future<void> _confirmSignOut(BuildContext context) async {
+    final bool didRequestSignOut = await PlatformAlertDialog(
+      title: Strings.logout,
+      content: Strings.logoutAreYouSure,
+      cancelActionText: Strings.cancel,
+      defaultActionText: Strings.logout,
+    ).show(context);
+    if (didRequestSignOut == true) {
+      _signOut(context);
+    }
+  }
+
+  Future<void> _signOut(BuildContext context) async {
+    try {
+      final FirebaseAuthService auth =
+          Provider.of<FirebaseAuthService>(context, listen: false);
+      await auth.signOut();
+    } on PlatformException catch (e) {
+      await PlatformExceptionAlertDialog(
+        title: Strings.logoutFailed,
+        exception: e,
+      ).show(context);
+    }
   }
 }
 
