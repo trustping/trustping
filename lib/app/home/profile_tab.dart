76e918aa53dc975a511c4f79c4495ac867da6037
Stefan Otte <stefan.otte@gmail.com>
2020-07-19 14:49:23 +0200

    Profile is now editable

--- lib/app/home/profile_tab.dart
+++ lib/app/home/profile_tab.dart
@@ -1,28 +1,32 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:trust_ping_app/app/home/models/user_profile_v2.dart';
+import 'package:trust_ping_app/app/onboarding/diagnosis_forms.dart';
+import 'package:trust_ping_app/app/onboarding/living_situation_forms.dart';
+import 'package:trust_ping_app/app/onboarding/therapy_forms.dart';
 import 'package:trust_ping_app/app/spaces.dart';
 import 'package:trust_ping_app/common_widgets/avatar.dart';
 import 'package:trust_ping_app/common_widgets/chips.dart';
 import 'package:trust_ping_app/common_widgets/platform_alert_dialog.dart';
 import 'package:trust_ping_app/common_widgets/platform_exception_alert_dialog.dart';
 import 'package:trust_ping_app/constants/strings.dart';
+import 'package:trust_ping_app/routing/router.gr.dart';
 import 'package:trust_ping_app/services/firebase_auth_service.dart';
 import 'package:trust_ping_app/services/firestore_database.dart';
 import 'package:trust_ping_app/theme.dart';
 
 class ProfileTab extends StatelessWidget {
-  const ProfileTab({
-    Key key,
-  }) : super(key: key);
+  const ProfileTab({Key key}) : super(key: key);
+  static final editIcon = Icon(Icons.edit, color: Style.textLightColor);
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        // _buildActionArea(context),
         _buildProfileDataArea(context),
       ],
     );
@@ -31,53 +35,82 @@ class ProfileTab extends StatelessWidget {
   Widget _buildProfileDataArea(BuildContext context) {
     final db = Provider.of<FirestoreDatabase>(context, listen: true);
 
-    final editIcon = Icon(Icons.edit, color: Style.textLightColor);
-
     return StreamBuilder<UserProfileV2>(
       stream: db.userProfileV2Stream(),
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           final profile = snapshot.data;
+          final _onNext = () => Navigator.of(context).pop();
+
           return Expanded(
             child: ListView(
               padding: EdgeInsets.all(24),
               children: <Widget>[
-                ListTile(
-                  leading: TPCircleAvatarWithBorder(
-                    radius: 28,
-                    child: Text(profile.name[0]),
-                    borderColor: Style.red,
-                    backgroundColor: Colors.white,
-                  ),
-                  title: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Style.title(profile.name),
-                      editIcon,
-                    ],
-                  ),
-                  subtitle: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Style.subtitle(profile.yearOfBirth.toString()),
-                      editIcon,
-                    ],
-                  ),
-                  // trailing: Icon(Icons.edit, color: Style.textLightColor),
+                Text("TODO move this button"),
+                _buildActionArea(context),
+                vspace16,
+                Divider(),
+                vspace16,
+                _bulidNameYearHeader(context, profile),
+                _buildSection(
+                  context,
+                  "Tumorart",
+                  profile.diagnosisCancerType,
+                  Style.reds,
+                  profile,
+                  () => DiagnosisCancerForm(profile: profile, onNext: _onNext),
                 ),
                 _buildSection(
-                    "Tumorart", profile.diagnosisCancerType, Style.reds),
-                _buildSection("Diagnose/Eigenschaften",
-                    profile.diagnosisCancerProperties, Style.reds),
+                  context,
+                  "Diagnose/Eigenschaften",
+                  profile.diagnosisCancerProperties,
+                  Style.reds,
+                  profile,
+                  () => DiagnosisPropertiesForm(
+                      profile: profile, onNext: _onNext),
+                ),
                 _buildSection(
-                    "Krankheitsphase", profile.diagnosisPhase, Style.reds),
-                _buildSection("Therapie", profile.therapyMethods, Style.blues),
+                  context,
+                  "Krankheitsphase",
+                  profile.diagnosisPhase,
+                  Style.reds,
+                  profile,
+                  () => DiagnosisPhaseForm(profile: profile, onNext: _onNext),
+                ),
                 _buildSection(
-                    "Nebenwirkungen", profile.therapySideEffects, Style.blues),
+                  context,
+                  "Therapie",
+                  profile.therapyMethods,
+                  Style.blues,
+                  profile,
+                  () => TherapyMethodForm(profile: profile, onNext: _onNext),
+                ),
                 _buildSection(
-                    "Situation", profile.situationGeneral, Style.yellows),
+                  context,
+                  "Nebenwirkungen",
+                  profile.therapySideEffects,
+                  Style.blues,
+                  profile,
+                  () =>
+                      TherapySideEffectForm(profile: profile, onNext: _onNext),
+                ),
                 _buildSection(
-                    "Interessen", profile.situationInterests, Style.yellows),
+                  context,
+                  "Situation",
+                  profile.situationGeneral,
+                  Style.yellows,
+                  profile,
+                  () => LivingSituationForm(profile: profile, onNext: _onNext),
+                ),
+                _buildSection(
+                  context,
+                  "Interessen",
+                  profile.situationInterests,
+                  Style.yellows,
+                  profile,
+                  () => LivingSituationInterestsForm(
+                      profile: profile, onNext: _onNext),
+                ),
               ],
             ),
           );
@@ -88,7 +121,48 @@ class ProfileTab extends StatelessWidget {
     );
   }
 
-  Widget _buildSection(String name, List<Item> items, List<Color> colors) {
+  ListTile _bulidNameYearHeader(BuildContext context, UserProfileV2 profile) {
+    final editButton = IconButton(
+      icon: editIcon,
+      onPressed: () {
+        Flushbar(
+          message: 'TODO not implemented yet',
+          duration: Duration(seconds: 3),
+        ).show(context);
+      },
+    );
+    return ListTile(
+      leading: TPCircleAvatarWithBorder(
+        radius: 28,
+        child: Text(profile.name[0]),
+        borderColor: Style.red,
+        backgroundColor: Colors.white,
+      ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Style.title(profile.name),
+          editButton,
+        ],
+      ),
+      subtitle: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Style.subtitle(profile.yearOfBirth.toString()),
+          editButton,
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSection(
+    BuildContext context,
+    String name,
+    List<Item> items,
+    List<Color> colors,
+    UserProfileV2 profile,
+    Function formBuilder,
+  ) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 12),
       child: Column(
@@ -98,7 +172,18 @@ class ProfileTab extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               Text(name, style: TextStyle(fontWeight: FontWeight.w600)),
-              Icon(Icons.edit, color: Style.textLightColor),
+              IconButton(
+                icon: Icon(Icons.edit, color: Style.textLightColor),
+                onPressed: () {
+                  ExtendedNavigator.of(context).pushNamed(
+                    Routes.profileEditPage,
+                    arguments: ProfileEditPageArguments(
+                      title: name,
+                      formBuilder: formBuilder,
+                    ),
+                  );
+                },
+              ),
             ],
           ),
           _buildWrap(items, colors),
@@ -135,6 +220,7 @@ class ProfileTab extends StatelessWidget {
     );
   }
 
+  // SIGNOUT
   Future<void> _confirmSignOut(BuildContext context) async {
     final bool didRequestSignOut = await PlatformAlertDialog(
       title: Strings.logout,
@@ -158,5 +244,28 @@ class ProfileTab extends StatelessWidget {
         exception: e,
       ).show(context);
     }
+  }
+}
+
+// =============================================================================
+class ProfileEditPage extends StatelessWidget {
+  final String title;
+  final Function formBuilder;
+
+  const ProfileEditPage({Key key, this.formBuilder, this.title})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView(
+        padding: EdgeInsets.all(32),
+        children: <Widget>[
+          Style.title(title),
+          formBuilder(),
+        ],
+      ),
+    );
   }
 }
